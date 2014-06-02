d4fc4b6ba59552d50e090cd679a032ad6093ff81
bgbhavesh <bgbhavesh@gmail.com>
2014-06-02 15:03:50 +0530

    updown

--- client/FaceTape.js
+++ client/FaceTape.js
@@ -1092,7 +1092,7 @@ Meteor.documentReady = documentReady;
         var newElement = null;
         var currentData = null;
         var showFlag = false;
-        upp ='<div id="back-top"><i class="level up icon">NEWER</i></div>'                  // go to upp
+        upp ='<div id="back-top"><i class="circle blank icon">NEWER</i></div>'                  // go to upp
         var element = $("#surveybig").append(upp);
         var totalimages=0;
         tot ='<div id="totalimages"><i class="level up icon">'+totalimages+'</i></div>'                  //total images

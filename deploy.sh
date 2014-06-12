f3b2ad2735f6b7449a8379e02210fa6d527b04bf
nicolsondsouza <nicolsondsouza@gmail.com>
2014-06-12 13:47:17 +0530

    hashrepublic facebook done

--- deploy.sh
+++ deploy.sh
@@ -32,4 +32,5 @@ export ROOT_URL='http://128.199.196.222:3000'
 export PORT=3000
 export METEOR_SETTINGS=$(cat ../../HashRepublic/hashsettings.json)
 export MAIL_URL='smtp://postmaster%40sandbox77539.mailgun.org:2l9s4cmzqic2@smtp.mailgun.org:587'
-pm2 start main.js
+pm2 flush
+pm2 start main.js  -i max

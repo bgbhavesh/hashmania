af3a2d8a34c83888091ec2b7c52b19bd7dac7497
nicolsondsouza <nicolsondsouza@gmail.com>
2014-06-23 19:58:19 +0530

    googleapi added

--- meteor/packages/youtap/package.js
+++ meteor/packages/youtap/package.js
@@ -6,6 +6,7 @@ Npm.depends({
     "node-schedule" : "0.1.13",
     // 'paypal-rest-sdk': '0.6.3',
     "fb" : "0.6.2",
+    "googleapis" : "0.7.0",
     // "passport" : "0.2.0",
     // "passport-facebook" : "1.0.3",
     // "form-data" : "0.1.2",

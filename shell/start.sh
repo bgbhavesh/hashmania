5fbf52b0ee780a0dec086149ef0d5e8ea07a2d5b
nicolsondsouza <nicolsondsouza@gmail.com>
2014-06-25 16:18:46 +0530

    shell updated

--- shell/start.sh
+++ shell/start.sh
@@ -3,21 +3,13 @@
 # It requires $domain.json config file and $domain dir with meteor bundle
 # The second argument is a TCP port for the app. It's optional
 # If it is not defined node.js will assign a random one
-cd $home
-ls
-echo "inside shell"
-exit
 
-domain='meteor.nicolsondsouza.com'
-#config=config/$domain.json
-dbname=$(echo $domain | cut -f1 -d.)
-#export METEOR_SETTINGS=`cat $config`
-export MONGO_URL='mongodb://localhost:27017/'$dbname
-export ROOT_URL='http://'$domain
 
-#if [ -n $2 ]
-#then
-  export PORT='80'
-#fi
+echo "one"
+echo "sdf"
 
-setsid meteor --production --port 80
+echo "wer"
+echo "onsdfe"
+
+echo "onwree"
+echo "onxcvze"

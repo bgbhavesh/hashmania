fdda3ace432b6e4a857883134e2d30a8a43db1b1
nicolsondsouza <nicolsondsouza@gmail.com>
2014-07-07 19:02:59 +0530

    72 optimized

--- meteor/client/FaceTape.js
+++ meteor/client/FaceTape.js
@@ -414,7 +414,17 @@ Meteor.startup(function () {
                 ErrorUpdate.insert({"error":error,"clientid":Session.get("clientid"),"date": new Date(),"side":"client","function" : "clientid.autorun"});
               } 
         });
+        
+        if(!DebugFace){
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+            ga('create', 'UA-52479656-1', 'auto');
+            ga('send', 'pageview');
+        }
+        
         // Deps.autorun(function () {
         //     try{
         //         actionArray = [];
@@ -4582,7 +4592,7 @@ SponserKeyword.find({}).observe({
     "added" : function(first){
         CouldTag.words.push(first.keyword);
         CouldTag.wordsSize.push(first.size);
-        CouldTag.onDraw();
+        CouldTag.onStart();
     }
 });
 function hideLikeButton(){

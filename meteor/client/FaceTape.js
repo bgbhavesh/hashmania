4fec77dbeec99150de65ae5e2cc4ef22ddb12343
bgbhavesh <bgbhavesh@gmail.com>
2014-07-07 17:29:48 +0530

    inset

--- meteor/client/FaceTape.js
+++ meteor/client/FaceTape.js
@@ -1194,14 +1194,14 @@ Meteor.documentReady = documentReady;
           }else{
             if(topTenLeaderRanking.indexOf(clientid)==-1){
                 html = '<div class="voting" clientid="' +clientid +'"votingid="' +id +'"linkId="' +newlinkId +'" style="left : ' +left +size +';top:' +top +size +';"> '
-                    +'<img src="' +pics +'" style="border-style: inset;">  '  
+                    +'<img src="' +pics +'" style="">  '  
                     +'<p class="triangle-right" style="top: -100%; left: -100%;">' +comment +'</p>'      
                     +'</div>'
                     +getCommentInArray(id,comment,ctop,cleft,commentid);
                   
             }else{
                 html = '<div class="voting" clientid="' +clientid +'"votingid="' +id +'"linkId="' +newlinkId +'" style="left : ' +left +size +';top:' +top +size +';"> '
-                    +'<img src="' +pics +'" style="border-style: inset;">  '  
+                    +'<img src="' +pics +'" style="">  '  
                     +'<p class="triangle-right" style="top: -100%; left: -100%;">' +comment +'</p>'      
                     +'</div>'
                     +getCommentInArray(id,comment,ctop,cleft,commentid);
@@ -3954,7 +3954,7 @@ function commentOneVote(){
         div.insertAdjacentHTML( 'afterend', html );
         var html = '<p class="triangle-right" style="top: -100%; left: -100%;">' +value +'</p>'; 
         div.insertAdjacentHTML( 'beforeend', html );
-        $(currImg).css({"border-style":"inset"});
+        // $(currImg).css({"border-style":"inset"});
         if(votingid){
         var VotesInsert = Votes.update({"_id":votingid},{$push :{"comment":value,"commenttop":currentTop,"commentleft":currentLeft,"commentid":Random.id()}});
         }

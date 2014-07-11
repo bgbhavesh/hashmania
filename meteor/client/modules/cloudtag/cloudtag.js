c43c77fc7a1de7e50b1a47eb5518d036ff25ce77
bgbhavesh <bgbhavesh@github.com>
2014-07-11 13:11:37 +0530

    d3

--- meteor/client/modules/cloudtag/cloudtag.js
+++ meteor/client/modules/cloudtag/cloudtag.js
@@ -36,10 +36,10 @@ var fill = d3.scale.category20();
 	function draw(words) {
 // console.log($("#keywords").width()+"keywords");
 		d3.select("#keywords").append("svg")
-			.attr("width", $("#keywords").width()*10/10)
-			.attr("height", $("#keywords").width()*15/10)
+			.attr("width", $("#keywords").width()*9/10)
+			.attr("height", $("#keywords").width()*12/10)
 			.append("g")
-			.attr("transform", "translate("+$("#keywords").position().left*2+","+$("#keywords").position().top*2+")scale(0.9)")
+			.attr("transform", "translate("+$("#keywords").position().left*2+","+$("#keywords").position().top*2+")scale(0.9,1.1) ")
 			.selectAll("text")
 			.data(words)
 			.enter().append("text")
